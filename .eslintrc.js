057e80c66d9c7e8f0f4fac0fc8edb73baf2ebe4d
Johnny-KK <32382053+Johnny-KK@users.noreply.github.com>
2023-03-02 08:51:36 +0000

    Build: 调整eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,10 @@
+process.env.ESLINT_TSCONFIG = 'tsconfig.json'
+
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    'vue/setup-compiler-macros': true,
-  },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-  },
-  plugins: ['vue', '@typescript-eslint'],
+  extends: '@antfu',
   rules: {
-    'vue/multi-word-component-names': 0,
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
   },
-};
+}
